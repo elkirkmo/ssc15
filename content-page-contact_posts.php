bd77ab5e46145317a8c7b69794320f886c4cf885
Chris Kirkham <me@chriskirkham.com>
2014-11-06 23:53:10 -0800

    More Contact Page Updates
    Adding Mobile Styles for Contact Page, added easier to manage buttons
    for mobile, links for FWS.

--- content-page-contact_posts.php
+++ content-page-contact_posts.php
@@ -7,20 +7,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php the_field('contact_image')?>
-	<header class="entry-header">
+	<?php 
+
+$image = get_field('contact_image');
+
+if( !empty($image) ): ?>
+
+	<img class="contact-img" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+<?php endif; ?>
+		<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<h2 class="contact-job-title"><?php the_field('job_title')?></h2>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_field('contact_address')?>
-		<div>
+		<div class="contact-bottom-buttons">
 			<div class="contact-number contact-button">
-				<a href="tel:<?php the_field("contact_country_code")?><?php the_field('contact_number'); ?>"><?php the_field('contact_number')?></a>
+				<a href="tel:<?php the_field("contact_country_code")?><?php the_field('contact_number'); ?>"><span class="contact-full"><?php the_field('contact_number')?></span><span class="contact-mobile">Call</span></a>
 			</div>
 			<div class="contact-email contact-button">
-				<a href="mailto:<?php the_field('contact_email')?>"><?php the_field('contact_email')?></a>
+				<a href="mailto:<?php the_field('contact_email')?>"><span class="contact-full"><?php the_field('contact_email')?></span><span class="contact-mobile">Email</span></a>
 			</div>
 		</div>
 	</div><!-- .entry-content -->

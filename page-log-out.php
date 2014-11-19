34cebcf9fe9fe47f549cba605550187717e9cd99
Chris Kirkham <me@chriskirkham.com>
2014-11-18 23:06:48 -0800

    Adding View/Edit Profile sections

--- page-log-out.php
+++ page-log-out.php
@@ -66,7 +66,15 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<h2>My Account</h2>
+				<header>
+					<h2> View Profile</h2>
+					<h2> Edit Profile</h2>
+				</header>
+			<div id="view_profile" class="left view-profile">
 			
+					<a href="<?php echo wp_logout_url('index.php'); ?>" title="Logout">Logout From Site</a>
+			</div>
+			<div id="edit_profile" class="right edit-profile">
 			<!--
 				Gender
 				First Name
@@ -233,10 +241,10 @@ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php _e('Sorry, no page matched your criteria.', 'profile'); ?>
     </p><!-- .no-data -->
 <?php endif; ?>
-				
+			</div>
       
 
-				<a href="<?php echo wp_logout_url('index.php'); ?>" title="Logout">Logout From Site</a>
+			
 				<?php if ( have_posts() ) : ?>
 
 			<?php ssc15_paging_nav(); ?>
